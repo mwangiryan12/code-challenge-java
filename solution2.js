48955b60f6a78963814f299cea33468b49b81073
mwangiryan12 <162281245+mwangiryan12@users.noreply.github.com>
2024-03-25 09:51:39 +0300

    solution2.js

--- solution2.js
+++ solution2.js
@@ -2,10 +2,13 @@
 let speed=80
 let speedlimit=70
 let points=Math.floor((speed-speedlimit)/5)
+//if speed is less than or equal to 7 print ok
 if(speed<=70){
     console.log('OK')
+//if points is > 12 print license suspended
 } else if(points>=12){
     console.log('license suspended')
+    //return output
 }else if(speed>70){
     console.log('points:' + points)
 }
