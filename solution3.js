bdb837cb448f61658cbcdac7b39d7c6cae0efb1c
mwangiryan12 <162281245+mwangiryan12@users.noreply.github.com>
2024-03-25 09:44:40 +0300

    solution3.js

--- solution3.js
+++ solution3.js
@@ -1,10 +1,16 @@
+//Get the input values
 const basicsalary=60000
 const benefits=6000
 function calculateNetSalary(){
+    //calculate the payee i.e(Tax)
     let payee = Math.floor((basicsalary + benefits) *0.25);
+    //calculate NHIFDeductions
      let NHIFDeductions=Math.floor(basicsalary * 0.02);
+    //calculate NSSFDeductions
      let NSSFDeductions=Math.floor(basicsalary * 0.12)
+    //calculate grosssalary
      let grosssalary=Math.floor(basicsalary + benefits)
+    //calculate netsalary
      let netsalary=grosssalary - (payee+NHIFDeductions+NSSFDeductions);
      return{
        grosssalary,
@@ -14,6 +20,7 @@ function calculateNetSalary(){
        netsalary,
      }
    }
+//output the net salary
    let output = calculateNetSalary(basicsalary, benefits)
    console.log('grosssalary:', output.grosssalary)
    console.log('NHIFDeductions:', output.NHIFDeductions)
