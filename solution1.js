83789bf26f8ec59092b0ea8c6f822a53d0072518
mwangiryan12 <162281245+mwangiryan12@users.noreply.github.com>
2024-03-25 10:05:41 +0300

    solution1.js

--- solution1.js
+++ solution1.js
@@ -3,19 +3,19 @@ console.log(Mynumber);
 function number(n){
 
 if(n>79){
-    console.log("Your Grade: A")
+    console.log("Your Grade: A") //print 'A' if marks are > 79
 }
 else if(n>=60 && n<=79){
-    console.log("Your Grade: B")
+    console.log("Your Grade: B") //print 'B' if marks are >=60 and <=79
 }
 else if(n>=50 && n<=59){
-    console.log("Your Grade: C")
+    console.log("Your Grade: C") //print 'C' if marks are >=50 and <=59
 }
 else if(n>=40 && n<=49){
-    console.log("Your Grade: D")
+    console.log("Your Grade: D")  //print 'D' if marks are >=40 and <=49
 }
 else{
-    console.log("Your Grade: E")
+    console.log("Your Grade: E")  //print 'E' if marks are below 39
 }
 return "marks:" + n
 }
